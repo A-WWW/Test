d37e83910596f1dbe86a0b5fbb60daed586cbd92
ASGWWWW <ASGWWW@gmail.com>
2021-08-08 21:28:28 +0300

    final

--- Parser.py
+++ Parser.py
@@ -1,6 +1,7 @@
 import bs4
 from bs4 import BeautifulSoup
 import requests
+import csv
 URL='https://electrica-shop.com.ua/c74-rozetki_i_viklyuchateli/filter/494p-1228.m-5/page/2'
 HEADERS={'User-Agent':'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.131 Mobile Safari/537.36', 'accept':'*/*'}
 def get(url, params=None):
@@ -16,14 +17,20 @@ def parse():
 def get_cont(html):
     soup = BeautifulSoup(html,'html.parser')
     items=soup.find_all(class_='products-txt-block')
-    print(type(items))
+    # print(type(items))
     prod = []
+    presence=[]
     for item in items:
-        prod.append({'title': item.find(class_='products-name').get_text(strip=True)})
-    print(prod)
-    print(len(prod))
-    #print(items)
+        prod.append({'Изделие': item.find(class_='products-name').get_text(strip=True),'Наличие': item.find(class_='products-quantity instock').get_text(strip=True)})
+    # print(items)
+    # print(prod)
+    print(f'Получено {len(prod)} товарных едениц')
+    for value in prod:
+        print(value)
+
+
 parse()
+
 
 
 
